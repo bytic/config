809003363cd03b427d5d2e1ec7c025f700086385
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-04 20:18:11 +0200

    initial commit

---
+++ src/FileParser/FileParserInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nip\Config\FileParser;
+
+/**
+ * Interface FileParserInterface
+ * @package Nip\Config\FileParser
+ */
+interface FileParserInterface
+{
+    /**
+     * Parses a file from `$path` and gets its contents as an array
+     *
+     *
+     * @return array
+     */
+    public function parse();
+
+    /**
+     * Returns an array of allowed file extensions for this parser
+     *
+     * @return array
+     */
+    public function getSupportedExtensions();
+}
---
+++ tests/AbstractTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nip\Config\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTest
+ */
+abstract class AbstractTest extends TestCase
+{
+    protected $object;
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+}
