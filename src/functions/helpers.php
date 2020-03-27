b96e21ee667ee21825c9a9c309229ca228507427
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-03-27 18:37:31 +0200

    fix config function to call also container directly

--- src/functions/helpers.php
+++ src/functions/helpers.php
@@ -1,24 +1,39 @@
 <?php
 
+use Nip\Container\Container;
+
 if (!function_exists('config')) {
+
     /**
      * Get / set the specified configuration value.
      *
      * If an array is passed as the key, we will assume you want to set an array of values.
      *
-     * @param  array|string $key
-     * @param  mixed $default
+     * @param array|string $key
+     * @param mixed $default
      * @return \Nip\Config\Config|mixed
      */
     function config($key = null, $default = null)
     {
-        if (is_null($key)) {
-            return app('config');
-        }
-        if (is_array($key)) {
-            return app('config')->set($key);
+        $container = function_exists('app') ? app() : Container::getInstance();
+        if (!$container) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            throw new Exception("No container was found for config function");
         }
 
-        return app('config')->get($key, $default);
+        $config = $container->get('config');
+        if (!$config) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            throw new Exception("No Config was found in the container");
+        }
+
+        if (is_null($key)) {
+            return $config;
+        }
+        if (is_array($key)) {
+            return $config->set($key);
+        }
+
+        return $config->get($key, $default);
     }
 }
